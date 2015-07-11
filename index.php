38438f079b1eb71a57f8c8833e404352193ed276
Tricia Nemiroff <tricianemiroff@Tricias-Mac.local>
2015-07-10 18:51:58 -0700

    practice commit message

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Nicole Nemiroff</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
